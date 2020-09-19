fc56bff4f669898a4cbc016290d19a057de2a7ad
Caio César Silva <caiocesarsilva08@gmail.com>
2020-09-19 12:14:09 -0300

    Verificação de algumas relações.

--- tp.c
+++ tp.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int tamanhoMatriz, *vetorAux, **matrizPrincipal, NUMERO_DE_LINHAS, NUMERO_DE_COLUNAS;
+int tamanhoMatriz, *vetorAux, **matrizPrincipal, **matrizTransposta, NUMERO_DE_LINHAS, NUMERO_DE_COLUNAS;
 FILE *arquivo_entrada;
 int valorAux, valorLinha, valorColuna;
 
@@ -21,6 +21,13 @@ void alocaMatrizPrincipal(){
     matrizPrincipal = (int **) malloc (sizeof(int *) * NUMERO_DE_LINHAS);
     for(int i = 0; i < NUMERO_DE_LINHAS; i++){
         matrizPrincipal[i] = (int *) malloc (sizeof(int) * NUMERO_DE_COLUNAS);
+    }
+}
+//Alocação da matriz transposta da mesma forma que a matriz principal, a usaremos para comparações
+void alocaMatrizTransposta(){
+    matrizTransposta = (int **) malloc (sizeof(int *) * NUMERO_DE_LINHAS);
+    for(int i = 0; i < NUMERO_DE_LINHAS; i++){
+        matrizTransposta[i] = (int *) malloc (sizeof(int) * NUMERO_DE_COLUNAS);
     }
 }
 
@@ -44,6 +51,15 @@ void preencheMatrizPrincipal(){
     }
 }
 
+//Armazena cada linha da matriz principal na coluna da matriz transposta (propriedades de matrizes)
+void preencheMatrizTransposta(){
+    for(int i = 0; i < NUMERO_DE_LINHAS; i++){
+        for(int j = 0; j < NUMERO_DE_COLUNAS; j++){
+            matrizTransposta[j][i] = matrizPrincipal[i][j];
+        }
+    }
+}
+
 //Imprimir matriz principal para teste
 void imprimeMatrizPrincipal(){
     for(int i = 0; i < NUMERO_DE_LINHAS; i++){
@@ -54,11 +70,31 @@ void imprimeMatrizPrincipal(){
     }
 }
 
+//Imprimir matriz transposta para teste
+void imprimeMatrizTransposta(){
+    for(int i = 0; i < NUMERO_DE_LINHAS; i++){
+        for(int j = 0; j < NUMERO_DE_COLUNAS; j++){
+            printf("%d", matrizTransposta[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+
 //Preenche matriz principal preenchendo com 0, para evitar erros
 void iniciaMatrizPrincipal(){
      for(int i = 0; i < NUMERO_DE_LINHAS; i++){
         for(int j = 0; j < NUMERO_DE_COLUNAS; j++){
             matrizPrincipal[i][j] = 0;
+        }
+    }
+}
+
+//Preenche matriz transposta preenchendo com 0, para evitar erros
+void iniciaMatrizTransposta(){
+     for(int i = 0; i < NUMERO_DE_LINHAS; i++){
+        for(int j = 0; j < NUMERO_DE_COLUNAS; j++){
+            matrizTransposta[i][j] = 0;
         }
     }
 }
@@ -70,11 +106,112 @@ void imprimeVetorAux(){
     }
 }
 
+//Verifica se a matriz principal é reflexiva
+void verificaReflexiva(){
+    int count = 0;
+    //Iteração pela diagonal da matriz
+    for(int i = 0; i < NUMERO_DE_LINHAS; i++){
+        for(int j = 0; j < NUMERO_DE_COLUNAS; j++){
+            if(i == j){
+                if(matrizPrincipal[i][j] == 1){
+                    count++;
+                }
+            }
+          
+        }
+    }
+    //Para ser reflexiva, o contador ter sido somado em todas as posições da diagonal (que é o mesmo número de linhas ou colunas)
+    if(count == NUMERO_DE_LINHAS){
+        printf("1. Reflexiva: V\n");
+    } else {
+        printf("1. Reflexiva: F\n");
+        //Caso não seja reflexiva, irei printar os pares que fazem com que essa condição não seja aceita.
+        for(int i = 0; i < NUMERO_DE_LINHAS; i++){
+            for(int j = 0; j < NUMERO_DE_COLUNAS; j++){
+                if(i == j){
+                    if(matrizPrincipal[i][j] != 1){
+                        printf("(%d, %d);", vetorAux[i], vetorAux[j]);
+                    }
+                }
+            }
+        }
+        printf("\n");
+    }
+}
+
+//Verifica se a matriz principal é irreflexiva
+void verificarIrreflexiva(){
+    int count = 0;
+    //Iteração pela diagonal da matriz
+    for(int i = 0; i < NUMERO_DE_LINHAS; i++){
+        for(int j = 0; j < NUMERO_DE_COLUNAS; j++){
+            if(i == j){
+                if(matrizPrincipal[i][j] != 1){
+                    count++;
+                }
+            }
+          
+        }
+    }
+
+    //Para ser irreflexiva, o contador NÃO pode ter sido somado em todas as posições da diagonal (que é o mesmo número de linhas ou colunas)
+    if(count == NUMERO_DE_LINHAS){
+        printf("2. Irreflexiva: V\n");
+    } else {
+        //Caso não seja irreflexiva, irei printar os pares que fazem com que essa condição não seja aceita.
+        printf("2. Irreflexiva: F\n");
+        for(int i = 0; i < NUMERO_DE_LINHAS; i++){
+            for(int j = 0; j < NUMERO_DE_COLUNAS; j++){
+                if(i == j){
+                    if(matrizPrincipal[i][j] == 1){
+                        printf("(%d, %d);", vetorAux[i], vetorAux[j]);
+                    }
+                }
+            }
+        }
+        printf("\n");
+    }
+}
+
+//Verifica se a matriz principal é simetrica
+void verificaSimetrica(){
+    int count = 1;
+    //Comparar se a matriz principal é igual a matriz transposta (propriedades de matrizes)
+    for(int i = 0; i < NUMERO_DE_LINHAS; i++){
+        for(int j = 0; j < NUMERO_DE_COLUNAS; j++){
+           if(matrizPrincipal[i][j] != matrizTransposta[i][j]){
+              count++;
+              break;
+           }
+        }
+    }
+
+    if(count == 1){
+        printf("Simetrica: V\n");
+    } else {
+        printf("Simetrica: F\n");
+    }
+}
+
 int main(){
-    arquivo_entrada = fopen("ex1.txt", "rt"); //Leitura do arquivo
+    arquivo_entrada = fopen("ex4.txt", "rt"); //Leitura do arquivo
     defineValoresAuxiliares();
+
     alocaMatrizPrincipal();
+    alocaMatrizTransposta();
+
     iniciaMatrizPrincipal();
+    iniciaMatrizTransposta();
+
     preencheMatrizPrincipal();
+    preencheMatrizTransposta();
+
     imprimeMatrizPrincipal();
+    printf("\n");
+    imprimeMatrizTransposta();
+
+    verificaReflexiva();
+    verificarIrreflexiva();
+
+    verificaSimetrica();
 }
